c363f861716805f1b97597e8cc2798f7b482bac8
Riya Khobragade <khobragaderiya11@gmail.com>
2026-05-25 16:00:29 +0530

    Added city , branch and passing year

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ student_name = "Riya"
 student_age = 19
 student_college = "PRIYADARSHINI COLLEGE OF ENGINEERING"
 student_marks = 85.66
-
+student_city = "Pune"
+student_branch = "Robotics & AI"
+studentpassingyear = 2028
 print("\n ----------Student Details----------")
 
 print("Student Name:",student_name)
 print("Student Age",student_age)
 print("Student College:", student_college)
 print("Student Marks:", student_marks)
+print("City:",student_city)
+print("Branch:",student_branch)
+print("Passing Year:",studentpassingyear)
 
 
 print("\n ----------Submittted By----------")
 
-print("Submitted By:Riya")
+print("Submitted By: Rya")
